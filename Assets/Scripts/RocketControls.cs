66198a7b5a7dcf3db3cdaa0a52d086e332fb2161
Saad Khan <skhan8@mail.einstein.yu.edu>
2018-01-27 18:20:09 -0500

    Added boundary enforcement  to player controller.
    Note: boundaries are currently hard coded.

--- Assets/Scripts/RocketControls.cs
+++ Assets/Scripts/RocketControls.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RocketControls : MonoBehaviour {
 	public float thrust = 10; // forward/backward force multiplier
 	public float torque = 1; // torque multiplier
+	public float bouncy = 3; // how much the level boundaries bounce the object 
+
 	private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
@@ -13,9 +15,30 @@ public class RocketControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		EnforceBoundaries ();
 		GetMovement ();
 	}
+	void EnforceBoundaries () {
+		// Applies an inward force field to the player if they attempt to leave the boundary.
+		float x = 8f;
+		float y = 4f;
+		// TODO: Get height and width of the scene from the camera.
+		Vector2 Position = gameObject.transform.position;
+		if (Position.x < -1 * x) {
+			rb.AddForce (Vector2.right * thrust * bouncy);
+		}
+		if (Position.x > x) {
+			rb.AddForce (Vector2.left * thrust * bouncy);
+		}
+		if (Position.y > y) {
+			rb.AddForce (Vector2.down * thrust * bouncy);
+		}
+		if (Position.y < -1 * y) {
+			rb.AddForce (Vector2.up * thrust * bouncy);
+		}
+	}
 	void GetMovement() {
+		// Responds to movement inputs by the player by rotating and or thrusting the rocket.
 		if (Input.GetButton("Vertical")) {
 			rb.AddRelativeForce (Vector2.up * thrust * Input.GetAxis("Vertical"));
 		}
