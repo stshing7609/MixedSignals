135dc451ff3e548357635eae191768c8f180b17e
Marcus Williams <2x4matrix@gmail.com>
2018-01-27 19:46:47 -0500

    Starter Cthullu stuff

---
+++ Assets/Scripts/cthulluBehavior.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cthulluBehavior : MonoBehaviour {
+
+	public static cthulluBehavior Instance;
+
+	static bool initialized = false;
+
+	public GameObject[] targets;
+
+	public cthulluMotor cm;
+
+	public int wanderIndex;
+
+	void Awake() {
+		if (!initialized) {
+			initialized = true;
+			Instance = this;
+		}
+	}
+
+	void Start(){
+		wanderIndex = Random.Range (0, targets.Length);
+
+	}
+
+	void Update(){
+		FindTarget ();
+		Wander ();
+	}
+
+	void FindTarget(){
+		cm.pos = targets[wanderIndex];
+		//if (cthulluX >= targetsX && cthulluY >= pt targetsY) {
+		if (cm.transform.position.x >= targets[wanderIndex].transform.position.x && cm.transform.position.y >= targets[wanderIndex].transform.position.y ) {
+			wanderIndex = Random.Range(0,targets.Length);
+			Debug.Log (wanderIndex);
+		}
+	}
+
+	float Distance(Vector3 pt1, Vector3 pt2) {
+		return Mathf.Sqrt(Mathf.Pow((pt2.x-pt1.x),2) + Mathf.Pow((pt2.y-pt1.y),2));
+	}
+
+	void TargetPowerUp() {
+	}
+
+	void TargetWinningTeam(){
+		
+	}
+
+	void TargetClosetTeam(){
+	}
+
+	void TargetTeamWithSmallestDistance(){
+	}
+
+	void Wander() {
+		cm.pos = targets[wanderIndex];
+		//if (cthulluX >= targetsX && cthulluY >= pt targetsY) {
+		if (cm.transform.position.x >= targets[wanderIndex].transform.position.x && cm.transform.position.y >= targets[wanderIndex].transform.position.y ) {
+			wanderIndex = Random.Range(0,targets.Length);
+			Debug.Log (wanderIndex);
+		}
+	}
+
+}
---
+++ Assets/Scripts/cthulluMotor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cthulluMotor : MonoBehaviour {
+
+	public static cthulluMotor Instance;
+
+	public float speed = 1.5f;
+	public Vector3 target;
+	public GameObject pos;
+
+	public GameObject[] targets;
+
+	private int pathIndex = 0;
+
+	private int pathDirection = 1;
+
+
+	void Awake() {
+		Instance = this;
+	}
+
+	void Start () {
+		target = transform.position;
+	}
+
+	void Update () {
+		ProcessMotion ();
+	}  
+
+	void ProcessMotion(){
+		target = pos.transform.position;
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+	}
+
+	void CurvePath(){
+		target = targets[pathIndex].transform.position;
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		if (transform.position.x >= targets[pathIndex].transform.position.x && transform.position.y >= targets[pathIndex].transform.position.y){
+			pathIndex += pathDirection;
+			if(pathIndex <= 0 || pathIndex >= targets.Length){
+				
+			}
+		}
+	}
+}
